4b13992e2d1125f0f9e0979219b9bba3c47dcd37
Coty Richardson <crichardson90@live.com>
2018-10-29 18:12:21 -0500

    added username ane and authentication

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,4 +1,5 @@
 class CartedProductsController < ApplicationController
+  before_action :authenticate_user!
   def index
     @carted_products = CartedProduct.where(is_removed: false, is_purchased: false)
   end
